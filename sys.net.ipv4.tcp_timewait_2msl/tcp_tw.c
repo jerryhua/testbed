a8fb0bc2327ef4d5230b76d22153de9a51a240d8
jerry.hua <huagb@appexnetworks.com>
2016-12-07 12:00:28 +0800

    Adjust code style

--- sys.net.ipv4.tcp_timewait_2msl/tcp_tw.c
+++ sys.net.ipv4.tcp_timewait_2msl/tcp_tw.c
@@ -1,35 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/netfilter.h>
-#include <linux/netfilter_ipv4.h>
 #include <linux/version.h>
-#include <linux/ip.h>
-#include <linux/if_ether.h>
-#include <net/net_namespace.h>
-#include <linux/hash.h>
-#include <linux/ip.h>
-#include <linux/if_arp.h>
-#include <net/protocol.h>
-#include <net/xfrm.h>
-#include <net/udp.h>
 #include <net/tcp.h>
-#include <net/flow.h>
 #include <linux/kernel.h>
-#include <linux/moduleparam.h>
 #include <linux/proc_fs.h>
 #include <net/inet_timewait_sock.h>
-
-static int tcp_timewait_2msl = TCP_TIMEWAIT_LEN / HZ;
-module_param(tcp_timewait_2msl, int, 0644);
-MODULE_PARM_DESC(tcp_timewait_2msl, "tcp time_wait length(seconds)");
 
 extern struct inet_timewait_death_row tcp_death_row;
 extern struct proc_dir_entry proc_root;
 
-static int old_twdr_period;
-
 #define NET_TCP_TIMEWAIT	1009
 
+static int tcp_timewait_2msl = TCP_TIMEWAIT_LEN / HZ;
+static int old_twdr_period;
 static struct ctl_table_header *tcp_tw_ctl;
 
 int proc_tcp_tw(ctl_table *table, int write, void __user *buffer, size_t *lenp,
@@ -43,7 +26,7 @@ int proc_tcp_tw(ctl_table *table, int write, void __user *buffer, size_t *lenp,
 		spin_lock(&twdr->death_lock);
 		twdr->period = tcp_timewait_2msl * HZ / INET_TWDR_TWKILL_SLOTS;
 		spin_unlock(&twdr->death_lock);	
-    	}
+	}
 	
 #if(0)
 	printk(KERN_ALERT"time_wait_len=%d second, twdr->period=%d ticks, HZ=%d,\n", 
@@ -57,36 +40,36 @@ int proc_tcp_tw(ctl_table *table, int write, void __user *buffer, size_t *lenp,
 static ctl_table tcp_tw_table[] =
 {
 	{
-        	.ctl_name       = NET_TCP_TIMEWAIT,
-	        .procname       = "tcp_timewait_2msl",
-        	.data           = &tcp_timewait_2msl,
-	        .maxlen         = sizeof(tcp_timewait_2msl),
-        	.mode           = 0644,
-	        .proc_handler   = &proc_tcp_tw
-    	},
-    	{ .ctl_name = 0 }
+		.ctl_name       = NET_TCP_TIMEWAIT,
+		.procname       = "tcp_timewait_2msl",
+		.data           = &tcp_timewait_2msl,
+		.maxlen         = sizeof(tcp_timewait_2msl),
+		.mode           = 0644,
+		.proc_handler   = &proc_tcp_tw
+	},
+	{ .ctl_name = 0 }
 };
 
 static ctl_table sys_net_ipv4_table[] =
 {
-    	{
-        	.procname       = "ipv4",
-	        .mode           = 0555,
-        	.child          = tcp_tw_table
-    	},
-    	{ .ctl_name = 0 }
+	{
+		.procname       = "ipv4",
+		.mode           = 0555,
+		.child          = tcp_tw_table
+	},
+	{ .ctl_name = 0 }
 };
 
 
 static ctl_table sys_net_table[] =
 {
 	{
-        	.ctl_name       = CTL_NET,
-	        .procname       = "net",
-        	.mode           = 0555,
-	        .child          = sys_net_ipv4_table
-    	},
-    	{ .ctl_name = 0 }
+		.ctl_name       = CTL_NET,
+		.procname       = "net",
+		.mode           = 0555,
+		.child          = sys_net_ipv4_table
+	},
+	{ .ctl_name = 0 }
 };
 
 
@@ -103,8 +86,8 @@ static int tw_init(void)
 	tcp_tw_ctl = register_sysctl_table(sys_net_table);
 	if (!tcp_tw_ctl) {
 		printk("register_sysctl_table error!\n");
-	        return -1;
-    	}
+		return -1;
+	}
 
 	return 0;
 }
